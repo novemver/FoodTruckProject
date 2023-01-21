df074d456599c2622679c71dad7a76319622498b
Dom <dominicjackss@gmail.com>
2023-01-20 16:08:31 -0800

    Here come the Food Trucks

---
+++ src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
@@ -0,0 +1,17 @@
+package com.skilldistillery.foodtruck.app;
+
+import com.skilldistillery.foodtruck.entities.Foodtruck;
+
+public class FoodTruckApp {
+
+//	fleet of Foodtrucks
+private	Foodtruck[] fleet;
+	
+	
+	public static void main(String[] args) {
+
+		
+		
+	}
+
+}
---
+++ src/com/skilldistillery/foodtruck/entities/Foodtruck.java
@@ -0,0 +1,7 @@
+package com.skilldistillery.foodtruck.entities;
+
+public class Foodtruck {
+	private static int nextTruckId;
+	private int id;
+}
+//instatiate the foodtrucks, look at parking lot! (;
