f5d8311aa5ac87d9347c94156e615389010e26e6
Dom <dominicjackss@gmail.com>
2023-01-23 07:55:51 -0800

    pushing incomplete project for review.
    clear
    git status

--- src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
+++ src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
@@ -1,17 +1,47 @@
 package com.skilldistillery.foodtruck.app;
 
+import java.util.Scanner;
+
 import com.skilldistillery.foodtruck.entities.Foodtruck;
+
+import solutions.Car;
 
 public class FoodTruckApp {
 
 //	fleet of Foodtrucks
-private	Foodtruck[] fleet;
-	
+private	Foodtruck[] fleet = new Foodtruck[5];
+
 	
 	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Welcome to The Food Truck App. \nPlease enter the name of the Food Truck. ");
+		
+		String name = scan.nextLine();
+		
+		System.out.println("Please enter the type of the food at this Food Truck. ");
+		
+		String foodType = scan.nextLine();
+		 
+		System.out.println("Please enter your rating of 1 (Worst) to 5 (Best) this Food Truck. ");
+		
+		int rating = scan.nextInt();
+		
+		   Foodtruck Foodtruck = new Foodtruck(name, foodType, rating);
+		  getFleet()[i] = Foodtruck;
+		   
+		
+	}
 
-		
-		
+
+	public Foodtruck[] getFleet() {
+		return fleet;
+	}
+
+
+	public void setFleet(Foodtruck[] fleet) {
+		this.fleet = fleet;
 	}
 
 }
--- src/com/skilldistillery/foodtruck/entities/Foodtruck.java
+++ src/com/skilldistillery/foodtruck/entities/Foodtruck.java
@@ -1,7 +1,73 @@
 package com.skilldistillery.foodtruck.entities;
 
 public class Foodtruck {
+//	This main FoodTruckApp does NOT pass inthe id value for any foodtruck. that will happen here
 	private static int nextTruckId;
-	private int id;
+
+	private String name;
+	private String foodType;
+	private int id = 0;
+	private int rating = 0;
+	private int counter;
+	
+	public Foodtruck(String name, String foodType, int rating) {
+		this.name = name;
+		this.foodType = foodType;
+		this.rating = rating;
+		Foodtruck.nextTruckId++;
+		this.id = nextTruckId;
+	}
+	public Foodtruck() {
+		id = counter++;
+	}
+//	 Print out the trucks info
+	public String toString() {
+		return "";
+	}
+
+	public Foodtruck[] getFoodTruck() {
+		Foodtruck[] nextTruckId = new Foodtruck[id];
+		
+	}
+	public static int getNextTruckId() {
+		return nextTruckId;
+	}
+	public static void setNextTruckId(int nextTruckId) {
+		Foodtruck.nextTruckId = nextTruckId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getFoodType() {
+		return foodType;
+	}
+	public void setFoodType(String foodType) {
+		this.foodType = foodType;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getRating() {
+		return rating;
+	}
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+	public int getCounter() {
+		return counter;
+	}
+	public void setCounter(int counter) {
+		this.counter = counter;
+	}
+
+
+
 }
-//instatiate the foodtrucks, look at parking lot! (;
+
+//instantiate the foodtrucks, look at parking lot! (;
